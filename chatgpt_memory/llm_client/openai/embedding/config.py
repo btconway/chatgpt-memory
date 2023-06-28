dffa4374b0011f19db92b46242beebc07e06a96d
btconway <ben@vntana.com>
2023-06-28 15:49:03 -0500

    Update config.py

--- chatgpt_memory/llm_client/openai/embedding/config.py
+++ chatgpt_memory/llm_client/openai/embedding/config.py
@@ -4,7 +4,8 @@ from chatgpt_memory.llm_client.config import LLMClientConfig
 
 
 class EmbeddingModels(Enum):
-    ada = "*-ada-*-001"
+    ada = "*-ada-*-002"
+    ada_002 = "text-embedding-ada-002"
     babbage = "*-babbage-*-001"
     curie = "*-curie-*-001"
     davinci = "*-davinci-*-001"
@@ -12,8 +13,8 @@ class EmbeddingModels(Enum):
 
 class EmbeddingConfig(LLMClientConfig):
     url: str = "https://api.openai.com/v1/embeddings"
-    batch_size: int = 64
+    batch_size: int = 128
     progress_bar: bool = False
-    model: str = EmbeddingModels.ada.value
+    model: str = EmbeddingModels.ada_002.value
     max_seq_len: int = 8191
     use_tiktoken: bool = False
